d5ac84e8936cc7eeb5c87d83a8c1a5e29cb97536
JP Caparas <jp@jpcaparas.com>
2017-08-13 22:25:47 +1200

    Add user seeder and fixtures

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
+        // Rest of users
+        factory(App\User::class, 10)->create();
     }
 }
