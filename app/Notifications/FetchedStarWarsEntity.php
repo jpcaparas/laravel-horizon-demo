d03cefb77784ccb160683715f979360268e83e6f
JP Caparas <jp@jpcaparas.com>
2017-08-13 22:26:32 +1200

    Add Fetch Star Wars entity job and complementary email notification

---
+++ app/Notifications/FetchedStarWarsEntity.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Zttp\Zttp;
+use Zttp\ZttpResponse;
+
+class FetchedStarWarsEntity extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * The star wars entity
+     *
+     * @var array
+     */
+    private $entity = [];
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param array $entity
+     */
+    public function __construct(array $entity)
+    {
+        $this->setEntity($entity);
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $response = json_encode($this->getEntity());
+
+        return (new MailMessage)
+                    ->line(sprintf('Info about Star Wars character: #%1$s.', json_encode($this->getEntity())))
+                    ->action('More details', url('/'))
+                    ->line(json_encode($response));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getEntity(): array
+    {
+        return $this->entity;
+    }
+
+    /**
+     * @param array $entity
+     */
+    public function setEntity(array $entity)
+    {
+        $this->entity = $entity;
+    }
+}
